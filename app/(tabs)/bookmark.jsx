ba602b59f26363b87a2150a36d3dbdcccba56852
CitrusSoda <dndhkaus1004@naver.com>
2024-11-11 22:45:12 +0900

    feat: 하단 탭 구현

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const BookMark = () => {
+  return (
+    <View>
+      <Text>북마크 페이지입니다.</Text>
+    </View>
+  );
+};
+
+export default BookMark;
---
+++ app/(tabs)/create.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Create = () => {
+  return (
+    <View>
+      <Text>작성 페이지입니다.</Text>
+    </View>
+  );
+};
+
+export default Create;
